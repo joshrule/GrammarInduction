e34835afc0b3d11624e8d38d22338d19287ff065
Eyal Dechter <eyaldechter@gmail.com>
2015-02-01 09:42:02 -0500

    More modifications to revert.

--- experiments/cogsci2014/plot.py
+++ experiments/cogsci2014/plot.py
@@ -11,14 +11,12 @@ def plot_count_list_probs(csv_file):
     assert(exists(csv_file))
 
     xs = np.loadtxt(csv_file, delimiter=",")
-    xs = np.exp(np.cumsum(3 * np.log(xs), 0))
     fig = plt.figure()
     ax = fig.add_subplot(111)
     for i in range(xs.shape[1]):
         ax.plot(range(1, len(xs)+1), 100*xs[:, i], linewidth=2, label=i)
 
     ax.set_xticks(range(10, 100, 10))
-    ax.set_xlim(1, 100)
     ax.set_ylim(0, 101)
     ax.set_xlabel("Highest number correctly reached when counting starting at 1.")
     ax.set_ylabel("Probability")
--- experiments/cogsci2014/test.pl
+++ experiments/cogsci2014/test.pl
@@ -72,10 +72,6 @@ gt_negInfty(A, B) :-
 
 
 
-succ_word(U, W) :- 
-    number_word(N, U), 
-    number_word(N1, W), 
-    N1 is N + 1.
 
 
 
@@ -85,8 +81,6 @@ get_successor_distribution(X, Distr1, N, MaxWordLength, Exp) :-
             (between(1, MaxWordLength, L), 
              get_successor_distribution_sub(X, D, N, L), 
              member(Y\LogP0, D), 
-             %% succ_word(U, X), 
-             %% Y\=U, 
              LogP is Exp*LogP0
             ), Distr), 
     assoc_to_lists(Distr, Ys, LogPs),
@@ -114,7 +108,7 @@ sample_successor(X, Y, N, MaxWordLength, Exp) :-
 
 succ_probability(W,Succ, P, N, MaxWordLength, Exp) :-
     get_successor_distribution(W, Distr, N, MaxWordLength, Exp), 
-    findall(P, (member(Succ\P, Distr)), Ps), 
+    findall(P, member(Succ\P, Distr), Ps), 
     P is sum(Ps).
 
 successor_distribution(W, Distr, N, MaxWordLength) :- 
